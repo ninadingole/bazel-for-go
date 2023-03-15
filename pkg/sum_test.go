57a4132de04a1f2616c5ea67acb73b1120ff5cde
Neenad Ingole <needna78@gmail.com>
2023-03-15 11:46:10 +0100

    add _test suffix to the test package

--- pkg/sum_test.go
+++ pkg/sum_test.go
@@ -1,10 +1,11 @@
-package pkg
+package pkg_test
 
 import (
+	"github.com/ninadingole/bazel-for-go/pkg"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_Sum(t *testing.T) {
-	assert.Equal(t, 3, Sum(1, 2))
+	assert.Equal(t, 3, pkg.Sum(1, 2))
 }
